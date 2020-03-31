e20ab5d9f41a73759216c16633a9585d691a2afb
Cole Johnson <cburnjohnson@gmail.com>
2020-03-30 17:32:44 -0700

    added svgs

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/pages/Home';
+import Login from './components/pages/Login';
 
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
     return (
         <GlobalProvider>
-            <Home />
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/login' component={Login} />
+                </Switch>
+            </Router>
         </GlobalProvider>
     );
 }
---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LoginForm from '../login/LoginForm';
+
+const Login = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default Login;
