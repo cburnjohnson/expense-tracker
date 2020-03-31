6ed59c4f07d481eab0bb6bef8bfe41d5e4d45762
Cole Johnson <cburnjohnson@gmail.com>
2020-03-30 17:00:26 -0700

    added transaction date

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,20 +1,24 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
 
 const Transaction = ({ transaction }) => {
     const context = useContext(GlobalContext);
 
     const { deleteTransaction } = context;
 
+    const date = new Date(transaction.createdAt);
+    const transactionDate = date.toDateString();
+
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.name}
             <span className={transaction.amount < 0 ? 'expense' : 'income'}>
-                {sign}${Math.abs(transaction.amount)}
+                {sign}${numberWithCommas(Math.abs(transaction.amount))}
             </span>
             <span className='transaction-date highlight'>
-                {transaction.date}
+                {transactionDate}
             </span>
             <button
                 className='delete-btn'
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -22,6 +23,14 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/transactions', require('./routes/transactions'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
