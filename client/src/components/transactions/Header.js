5c1c0b80e6cb0891e9ffd5eb294c3dfc8f7454b3
Cole Johnson <cburnjohnson@gmail.com>
2020-03-31 18:47:02 -0700

    logout buton

--- client/src/components/transactions/Header.js
+++ client/src/components/transactions/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <h2>
-            <span className='highlight'>E</span>xp
-            <span className='highlight'>e</span>ns
-            <span className='highlight'>e</span> Tr
-            <span className='highlight'>a</span>ck
-            <span className='highlight'>e</span>r
-        </h2>
+        <>
+            <h2>
+                <span className='highlight'>E</span>xp
+                <span className='highlight'>e</span>ns
+                <span className='highlight'>e</span> Tr
+                <span className='highlight'>a</span>ck
+                <span className='highlight'>e</span>r
+            </h2>
+            <button id='logout'>
+                <span className='logout-text'>Logout</span>
+                <FontAwesomeIcon icon={faSignOutAlt} className='logout-icon' />
+            </button>
+        </>
     );
 };
 
